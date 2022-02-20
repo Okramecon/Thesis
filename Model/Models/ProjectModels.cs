e637d93de6c4dad18c973c81ad0a7d02a9409605
Akram Maflinov <akram.maflinov@timelysoft.net>
2022-02-20 17:28:18 +0600

    Added transfer models for entites

--- Model/Models/ProjectModels.cs
+++ Model/Models/ProjectModels.cs
@@ -3,28 +3,34 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Model.Models
 {
-    public static class DepartmentModels
+    public static class ProjectModels
     {
-        public class GetDepartmentModel : IIdHasInt
+        public class GetProjectModel : IIdHasInt
         {
             public int Id { get; set; }
+            [MaxLength(20)]
             public string Title { get; set; }
+            [MaxLength(100)]
             public string Summary { get; set; }
+            public int DepartmentId { get; set; }
 
-            //public ICollection<Project> Projects { get; set; }
+            //public ICollection<Board> Boards { get; set; }
         }
 
-        public class AddDepartmentModel
+        public class AddProjectModel
         {
             public string Title { get; set; }
+            [MaxLength(100)]
             public string Summary { get; set; }
+            public int DepartmentId { get; set; }
         }
 
-        public class EditDepartmentModel : IIdHasInt
+        public class EditProjectModel : IIdHasInt
         {
             [Required]
             public int Id { get; set; }
             public string Title { get; set; }
+            [MaxLength(100)]
             public string Summary { get; set; }
         }
     }
---
+++ Model/Models/UserStoryModels.cs
@@ -0,0 +1,33 @@
+﻿using Common.Interfaces;
+using System;
+
+namespace Model.Models
+{
+    public static class UserStoryModels
+    {
+        public class GetUserStoryModel : IIdHasInt
+        {
+            public int Id { get; set; }
+            public string Title { get; set; }
+            public string Details { get; set; }
+            public DateTime CreatedDateTime { get; set; }
+            public int BoardId { get; set; }
+
+            //public ICollection<Ticket> Tickets { get; set; }
+        }
+
+        public class AddUserStoryModel
+        {
+            public string Title { get; set; }
+            public string Details { get; set; }
+            public int BoardId { get; set; }
+        }
+
+        public class EditUserStoryModel : IIdHasInt
+        {
+            public int Id { get; set; }
+            public string Title { get; set; }
+            public string Details { get; set; }
+        }
+    }
+}
