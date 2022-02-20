ab208ea58fefbaa8b6fd0895c8662e061702ea59
Akram Maflinov <akram.maflinov@timelysoft.net>
2022-02-20 17:09:31 +0600

    Added Department controller

---
+++ API/Extensions/ConfigureServicesExtensions.cs
@@ -0,0 +1,18 @@
+﻿using BLL.Services;
+using DAL.EF;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Extensions
+{
+    public static class ConfigureServicesExtensions
+    {
+        public static void AddServices(this IServiceCollection services)
+        {
+            services.AddScoped<DepartmentService>();
+        }
+    }
+}
---
+++ Model/Models/DepartmentModels.cs
@@ -0,0 +1,36 @@
+﻿using Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Models
+{
+    public static class DepartmentModels
+    {
+        public class GetDepartmentModel : IIdHasInt
+        {
+            public int Id { get; set; }
+            public string Title { get; set; }
+            public string Summary { get; set; }
+
+            //public ICollection<Project> Projects { get; set; }
+        }
+
+        public class AddDepartmentModel
+        {
+            public string Title { get; set; }
+            public string Summary { get; set; }
+        }
+
+        public class EditDepartmentModel : IIdHasInt
+        {
+            [Required]
+            public int Id { get; set; }
+            public string Title { get; set; }
+            public string Summary { get; set; }
+        }
+    }
+}
